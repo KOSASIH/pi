f1021c737561bcd3b5fea0a9ecc0a51e256dc3f2
KOSASIH <kosasihg88@gmail.com>
2025-03-06 13:05:23 +0700

    Update PiCoinStabilization.sol

--- economic_stabilization/PiCoinStabilization.sol
+++ economic_stabilization/PiCoinStabilization.sol
@@ -5,15 +5,14 @@ contract PiCoinStabilization {
     string public name = "Pi Coin";
     string public symbol = "PI";
     uint8 public decimals = 18;
-    uint256 public totalSupply;
+    uint256 public totalSupply = 100000000000 * 10 ** uint256(decimals); // Fixed total supply
     uint256 public pegValue = 314159 * 10 ** uint256(decimals); // Pegged value in wei
     mapping(address => uint256) public balanceOf;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event PegAdjusted(uint256 newPegValue);
 
-    constructor(uint256 initialSupply) {
-        totalSupply = initialSupply * 10 ** uint256(decimals);
+    constructor() {
         balanceOf[msg.sender] = totalSupply; // Assign all tokens to the contract creator
     }
 
